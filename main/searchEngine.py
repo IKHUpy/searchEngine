16b73d0d2b6bfb8da486787756387c03020a0d82
IKHUpy <72260774+IKHUpy@users.noreply.github.com>
2023-04-24 14:23:33 +0800

    Update searchEngine.py

--- main/searchEngine.py
+++ main/searchEngine.py
@@ -8,9 +8,13 @@ def main():
     choice = input(
         f'What will be the data test type?\n0 - {choices[0]}\n2 - {choices[1]}\ninput here: ')
     if choice == '0':
-        text = ''
+        text = input('text to tokenize: ')
+
     elif choice == '1':
         text = ''
+        dir_input = input('dir to tokenize: ')
+        with open(str(dir_input), 'r') as f:
+            text = f.read()
 
     return
 
